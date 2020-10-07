43bb55adb25369e9276c51efe5c4ab628e28d20a
yasirkhan1301 <mr_yasir_khan52@yahoo.com>
2020-10-07 15:27:42 +0500

    Mongo Db implementation complete

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser")
-const request = require("request")
+const request = require("request");
+const mongoose = require('mongoose');
+
 
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}));
 
-// app.get("/", function(request, response)
-// {
-// response.sendFile(__dirname +"/index.html")
-// })
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/email_dbs';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var Schema = mongoose.Schema;
+
+var email = new Schema({
+  a_string: String,
+});
+
+
+// Compile model from schema
+var email = mongoose.model('emails', email );
 
 app.route('/')
   .get(function (req, res) {
@@ -35,10 +52,21 @@ app.get('/:custom', function (req, res)
  app.post('/search', function(req,res)
 {
   console.log(req.body.searching);
+
 })
 app.post('/newsletter',function(req,res)
 {
-  console.log(req.body.email);
+
+
+    var email_instance = new email({ name: req.body.email });
+
+  // Save the new model instance, passing a callback
+  email_instance.save(function (err) {
+    if (err) return handleError(err);
+    console.log("Saved");
+
+    
+  });
 })
 
 
